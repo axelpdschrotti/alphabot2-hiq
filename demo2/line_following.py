b274eb1a3651e9d5863e9aff32b4c6bd20360355
Idris Sahil <idris.sahil@hiq.se>
2025-02-13 14:15:15 +0100

    changed 90deg time

--- demo2/line_following.py
+++ demo2/line_following.py
@@ -15,7 +15,7 @@ THRESHOLD = 700
 #MAXDIFF = 200
 SENSOR_COUNT = 5  # Number of sensors
 # Turn timing configuration (calibrate these values)
-TURN_90_TIME = 0.4    # Time needed for 90° turn at current speed
+TURN_90_TIME = 0.6    # Time needed for 90° turn at current speed
 
 
 
