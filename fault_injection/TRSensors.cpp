2beaec4ee03bc2d6efb1dcb6a4696b355ce4fd77
Axel Schrotti <axel.schrotti@hiq.se>
2025-02-21 16:37:33 +0100

    fixed syntax

--- fault_injection/TRSensors.cpp
+++ fault_injection/TRSensors.cpp
@@ -11,72 +11,83 @@
 #define BUTTON 7
 #define ADDR_SIZE 8
 
-public class TRSensor {
+class TRSensor {
+public:
     int numSensors;
     int lastValue;
 
+    // Constructor
     TRSensor(int sensors) {
         if (gpioInitialise() < 0) {
             std::cerr << "Error initializing pigpio!" << std::endl;
-            return -1;
+            exit(1); // Use exit() instead of return
         }
         numSensors = sensors;
         lastValue = 0;
+
+        // Set GPIO modes
+        gpioSetMode(CS, PI_OUTPUT);
+        gpioSetMode(CLOCK, PI_OUTPUT);
+        gpioSetMode(ADDRESS, PI_OUTPUT);
+        gpioSetMode(DATAOUT, PI_INPUT);
     }
 
+    // Destructor
     ~TRSensor() {
         gpioTerminate();
-    };
+    }
 
+    // Function to read analog sensor values
     std::vector<int> analogRead() {
-        std::vector<int> value;
-        for (int i = 0; i <= numSensors; i ++) {
-            value.push_back(0);
-        }
-        for (int i = 0; i <= numSensors; i ++) {
-            gpioWrite(CS, 0);
-            for (int j = 0; i <= ADDR_SIZE; i ++ ) {
-                if (i < 4) {
-                    if (((j) >> (3 - i)) & 0x01) {
-                        gpioWrite(ADDRESS, 1);
-                    }
-                    else {
-                        gpioWrite(ADDRESS, 0);
-                    }
-                }
-                else {
+        std::vector<int> value(numSensors, 0);
+
+        for (int i = 0; i < numSensors; i++) {  // Fixed loop condition
+            gpioWrite(CS, 0);  // Start communication
+
+            for (int j = 0; j < ADDR_SIZE; j++) {  // Fixed loop variable
+                if (j < 4) {
+                    gpioWrite(ADDRESS, ((i >> (3 - j)) & 0x01));
+                } else {
                     gpioWrite(ADDRESS, 0);
                 }
-                value.at(j) <<= 1;
+
+                value[i] <<= 1;
                 if (gpioRead(DATAOUT)) {
-                    value.at(j) |= 0x01;
+                    value[i] |= 0x01;
                 }
+
                 gpioWrite(CLOCK, 1);
                 gpioWrite(CLOCK, 0);
             }
-            std::sleep(0.1);
-            gpioWrite(CS, 1);
+
+            std::this_thread::sleep_for(std::chrono::milliseconds(100));  // Fixed sleep syntax
+            gpioWrite(CS, 1);  // Stop communication
         }
-        for (int i = 0; i < 6; i ++){
-            value.at(i) >>= 2;
+
+        // Right-shift values
+        for (int i = 0; i < numSensors; i++) {
+            value[i] >>= 2;
         }
 
         return value;
     }
-
 };
-
 
 int main() {
     int numSensors = 5;
-    TRSensor sensor = TRSensor::TRSensor(numSensors);
-    std::vector<int> s;
-    while(true) {
-        s = sensor.analogRead();
-        for (int i = 0; i <= numSensors; i++) {
-            std::cout << std::to_string(s.at(i)) << ", ";
+    TRSensor sensor(numSensors);
+
+    while (true) {
+        std::vector<int> s = sensor.analogRead();
+
+        for (int i = 0; i < numSensors; i++) {  // Fixed loop condition
+            std::cout << s[i] << ", ";
         }
-        std::endl();
+
+        std::cout << std::endl;  // Fixed std::endl usage
+
+        std::this_thread::sleep_for(std::chrono::milliseconds(500));  // 500ms delay
     }
+
     return 0;
 }
