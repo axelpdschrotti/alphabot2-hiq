775cea5ed1f45fe1ec95044c72545a593442644a
idrissahil <idrissahil3@gmail.com>
2025-03-18 13:55:19 +0100

    changes to camera script picamera

--- demo4/camera_robot.py
+++ demo4/camera_robot.py
@@ -1,30 +1,10 @@
-import cv2
+from picamera2 import Picamera2
 
-def main():
-    # Open the default camera (device 0)
-    cap = cv2.VideoCapture(0)
-    if not cap.isOpened():
-        print("Error: Cannot open camera")
-        return
+picam2 = Picamera2()
+picam2.start()
+picam2.capture_file("test.jpg")
 
-    while True:
-        ret, frame = cap.read()  # Capture frame-by-frame
-        if not ret:
-            print("Error: Cannot receive frame")
-            break
-
-        cv2.imshow('Alphabot2 Pi Camera', frame)
-
-        key = cv2.waitKey(1)
-        if key == ord('q'):
-            break
-        elif key == ord('s'):  # Press 's' to save a snapshot
-            cv2.imwrite('snapshot.jpg', frame)
-            print("Snapshot saved as snapshot.jpg")
-
-    # Release the capture and close any OpenCV windows
-    cap.release()
-    cv2.destroyAllWindows()
-
-if __name__ == '__main__':
-    main()
+frame = picam2.capture_array()
+cv2.imshow("Camera", frame)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
